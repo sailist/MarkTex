48b582604cb85b785b913197d2aff6c0b8032b22
sailist <sailist@outlook.com>
2021-09-28 14:08:11 +0800

    update example

--- run_example.py
+++ run_example.py
@@ -3,10 +3,6 @@ import os
 
 base = 'marktex/example'
 fs = os.listdir(base)
-fs = [os.path.join(base, f) for f in fs if f.endswith('.md')]
-
-
+fs = [os.path.join(base, f) for f in fs if f.endswith('.md')][0:1]
 
 api.convert(*fs, output_dir='./outputs')  # 全部输出到该目录下 # 不推荐设置缓存图片目录
-# api.convert(*fs)  # 根据各自的 md 文件所在的目录，输出到相应的 output 子目录下，图片缓存在 output/imgs 下
-
